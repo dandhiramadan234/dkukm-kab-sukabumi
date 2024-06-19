7f4ee8d959bb24b3138c2724b184af7ee1d19b62
dandhiramadan234 <dandhiramadan234@gmail.com>
2024-06-19 20:45:54 +0700

    migration nullable

--- database/migrations/2024_06_18_164947_create_umkms_table.php
+++ database/migrations/2024_06_18_164947_create_umkms_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,42 +12,42 @@ return new class extends Migration
     {
         Schema::create('umkms', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_umkm');
-            $table->string('nama_pemilik');
-            $table->string('nik');
-            $table->string('no_handphone');
-            $table->string('email');
-            $table->string('tempat_lahir');
-            $table->date('tanggal_lahir');
-            $table->string('umur');
-            $table->string('jenis_kelamin');
-            $table->string('status_perkawinan');
-            $table->string('pendidikan');
-            $table->string('alamat_rumah');
-            $table->string('alamat_usaha');
-            $table->string('jenis_sektor');
-            $table->string('jenis_usaha');
-            $table->string('bentuk_hukum_perusahaan');
-            $table->string('no_ijin_usaha');
-            $table->json('kepemilikan_ijin_usaha');
-            $table->integer('tenaga_kerja_tetap_perempuan');
-            $table->integer('tenaga_kerja_tetap_laki_laki');
-            $table->integer('tenaga_kerja_lepas_perempuan');
-            $table->integer('tenaga_kerja_lepas_laki_laki');
-            $table->string('modal_awal_usaha');
-            $table->string('volume_produksi');
-            $table->string('satuan_volume_produksi');
-            $table->string('omzet_penjualan');
-            $table->string('quantity_penjualan');
-            $table->string('keuntungan_bersih');
-            $table->string('asset_tanah_bangunan');
-            $table->string('asset_mesin_peralatan');
-            $table->string('asset_kendaraan');
-            $table->json('daerah_pemasaran');
-            $table->json('kemitraan');
-            $table->string('pelatihan');
-            $table->string('status_umkm');
-            $table->string('gen');
+            $table->string('nama_umkm')->nullable();
+            $table->string('nama_pemilik')->nullable();
+            $table->string('nik')->nullable();
+            $table->string('no_handphone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('tempat_lahir')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('umur')->nullable();
+            $table->string('jenis_kelamin')->nullable();
+            $table->string('status_perkawinan')->nullable();
+            $table->string('pendidikan')->nullable();
+            $table->string('alamat_rumah')->nullable();
+            $table->string('alamat_usaha')->nullable();
+            $table->string('jenis_sektor')->nullable();
+            $table->string('jenis_usaha')->nullable();
+            $table->string('bentuk_hukum_perusahaan')->nullable();
+            $table->string('no_ijin_usaha')->nullable();
+            $table->json('kepemilikan_ijin_usaha')->nullable();
+            $table->integer('tenaga_kerja_tetap_perempuan')->nullable();
+            $table->integer('tenaga_kerja_tetap_laki_laki')->nullable();
+            $table->integer('tenaga_kerja_lepas_perempuan')->nullable();
+            $table->integer('tenaga_kerja_lepas_laki_laki')->nullable();
+            $table->string('modal_awal_usaha')->nullable();
+            $table->string('volume_produksi')->nullable();
+            $table->string('satuan_volume_produksi')->nullable();
+            $table->string('omzet_penjualan')->nullable();
+            $table->string('quantity_penjualan')->nullable();
+            $table->string('keuntungan_bersih')->nullable();
+            $table->string('asset_tanah_bangunan')->nullable();
+            $table->string('asset_mesin_peralatan')->nullable();
+            $table->string('asset_kendaraan')->nullable();
+            $table->json('daerah_pemasaran')->nullable();
+            $table->json('kemitraan')->nullable();
+            $table->string('pelatihan')->nullable();
+            $table->string('status_umkm')->nullable();
+            $table->string('gen')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
